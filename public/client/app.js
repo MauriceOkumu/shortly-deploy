b3b0d7314360879346d2b9fbe27511878a1275d9
Hack Reactor Students <accounts@hackreactor.com>
2017-01-18 15:34:00 -0800

    remove x from tests

--- public/client/app.js
+++ public/client/app.js
@@ -1,6 +1,5 @@
 window.Shortly = Backbone.View.extend({
   template: Templates.layout,
-//  TEST TEST TEST
   events: {
     'click li a.index': 'renderIndexView',
     'click li a.create': 'renderCreateView'
